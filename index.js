0b5ef30ead1c330e4ce8163c0d3f3c124858d1c3
nolombardo <nolan.lombardo11@gmail.com>
2025-09-16 09:38:15 -0400

    feat/post-and-get-usernames

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config();
 
-app.use(cors())
-app.use(express.static('public'))
+app.use(cors());
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+const users = [];
+
+// Post and Get usernames
+app.route("/api/users").post(bodyParser.urlencoded({extended: false}), (req,res) => {
+  const newName = req.body.username;
+  console.log(newName);
+  const user = users.find(({username}) => username === newName);
+  if (user) {
+    res.json(user);
+  } else {
+    userObject = {username: newName, _id: String(users.length)}
+    users.push(userObject);
+    res.json(userObject);
+  }
+}).get((req,res) => {
+  res.json(users);
+  console.log(users);
 });
 
 
 
-
-
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log('Your app is listening on port ' + listener.address().port);
+});
